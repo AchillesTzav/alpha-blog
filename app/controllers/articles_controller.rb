b8f66dc605998b6c1c8bb069abc9a17f3cf32da4
Achilles Tzavalas <achilles.tzavalas@gmail.com>
2024-10-31 01:33:19 +0200

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+  #before action runs the given action in this case :find_article
+  #in every method unless you specify
+  #in which methods you want to run the action before these methods
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article= Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:notice] = "Article was created successfully!"
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:info] = "Article was updated successfully!"
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:warning] = "Alert deleted successfully!"
+    redirect_to articles_path status: :see_other
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+    def find_article
+      @article = Article.find(params[:id])
+    end
+end

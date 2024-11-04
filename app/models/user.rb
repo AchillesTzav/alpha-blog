9104a7ed7de363069153cc4a57cb40531022728a
Achilles <achilles.tzavalas@gmail.com>
2024-11-04 17:01:22 +0200

    regex validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
-  validates: :username, presence: true, length: { minimun: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
-  validates: :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }
+  validates: :username, presence: true,
+              length: { minimun: 3, maximum: 25 },
+              uniqueness: { case_sensitive: false }
+
+  #constant variable , it will not change
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates: :email, presence: true,
+              length: { maximum: 105 },
+              uniqueness: { case_sensitive: false },
+              format: { with: VALID_EMAIL_REGEX }
 end
